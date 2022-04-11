5a5e9d2113164eaead73fdef3bb0668231994646
aishwaryapradeep01 <87689930+aishwaryapradeep01@users.noreply.github.com>
2022-04-11 21:52:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
-
+require('dotenv').config()
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET
